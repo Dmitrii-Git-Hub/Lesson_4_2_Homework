d26b43f87e363fcaa75dc024681a0de4ad42017b
Dima <nhr@bk.ru>
2022-09-21 12:50:46 +0700

    First App

---
+++ FirstApp/Program.cs
@@ -0,0 +1,28 @@
+﻿/*void Insert(int a)
+{
+    Console.WriteLine("Вставить" + a);
+}*/
+
+int CreatNumber()
+{
+    return new Random().Next(10,100);
+}
+
+int MaxDigit(int number)
+{
+    int Decimal = number/10;
+    int ed = number % 10;
+
+    if (Decimal >= ed)
+    {
+        return Decimal;
+    }
+    else
+    {
+        return ed;
+    }
+}
+
+int a = CreatNumber();
+Console.WriteLine(a);
+Console.WriteLine(MaxDigit(a));
